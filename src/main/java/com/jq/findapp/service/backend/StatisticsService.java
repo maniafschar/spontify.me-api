63d51ca64236b50c2f989f072132161edc8a4442
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2022-10-13 21:55:03 +0200

    timestamp in statistics.json

--- src/main/java/com/jq/findapp/service/backend/StatisticsService.java
+++ src/main/java/com/jq/findapp/service/backend/StatisticsService.java
@@ -2,8 +2,8 @@ package com.jq.findapp.service.backend;
 
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
@@ -21,12 +21,13 @@ public class StatisticsService {
 	private Repository repository;
 
 	public void update() throws Exception {
-		final Map<String, List<Object[]>> data = new HashMap<>();
+		final Map<String, Object> data = new HashMap<>();
 		final QueryParams params = new QueryParams(Query.misc_statsUser);
 		params.setLimit(0);
 		data.put("user", repository.list(params).getList());
 		params.setQuery(Query.misc_statsLog);
 		data.put("log", repository.list(params).getList());
+		data.put("update", Instant.now().toString());
 		IOUtils.write(new ObjectMapper().writeValueAsString(data), new FileOutputStream("statistics.json"),
 				StandardCharsets.UTF_8);
 	}
