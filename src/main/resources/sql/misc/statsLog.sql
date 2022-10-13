6702318c297f9685bf8565e19c3c61165ed98012
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2022-10-13 23:01:19 +0200

    rm ads from stats

--- src/main/resources/sql/misc/statsLog.sql
+++ src/main/resources/sql/misc/statsLog.sql
@@ -1,11 +1,11 @@
 SELECT
-	cast(log.time-5/10 as integer) as time,
+	cast((log.time-5)/10 as integer) as time,
 	count(*) as count,
 	concat(YEAR(log.createdAt),'-',MONTH(log.createdAt),'-',DAY(log.createdAt)) as date
 FROM
 	Log log
 WHERE
-	log.uri not like '/support/%' and log.createdAt>'2022-09-01'
+	log.uri<>'ad' and log.uri not like '/support/%' and log.createdAt>'2022-09-01'
 GROUP BY
 	date,
 	time
