91bd18044d928f747f8c483693163379dbd15fb5
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2022-10-22 14:05:25 +0200

    log import double quote fix

--- src/test/java/com/jq/findapp/service/ExternalServiceTest.java
+++ src/test/java/com/jq/findapp/service/ExternalServiceTest.java
@@ -19,28 +19,28 @@ public class ExternalServiceTest {
 	@Test
 	public void importLog() throws ParseException {
 		// given
-		final String line = "217.248.54.4 - - [13/Sep/2022:21:04:54 +0200] \"GET /?ad=google&gclid=EAIaIQobChMIzuS_-rqS-gIVwW3TCh13FQDiEAEYASAAEgIp1PD_BwE HTTP/1.1\" 200 6749 \"https://abc.de\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Mobile/15E148 Safari/604.1\"";
+		final String line = "134.96.235.1 - - [22/Oct/2022:06:58:54 +0200] \"GET /js/main.js HTTP/1.1\" 200 4654 \"https://blog.spontify.me/\" \"\\\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.18 Safari/537.36\\\"\"";
 		final Pattern pattern = Pattern.compile(
 				"([\\d.]+) (\\S) (\\S) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(\\w+) ([^ ]*) ([^\"]*)\" (\\d+) (\\d+) \"([^\"]*)\" \"([^\"]*)\"");
-		final Matcher m = pattern.matcher(line);
+		final Matcher m = pattern.matcher(line.replaceAll("\\\\\"", ""));
 		final DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.ENGLISH);
 
 		// when
 		m.find();
 
 		// then
-		assertEquals("217.248.54.4", m.group(1));
-		assertEquals("13/Sep/2022:21:04:54 +0200", m.group(4));
+		assertEquals("134.96.235.1", m.group(1));
+		assertEquals("22/Oct/2022:06:58:54 +0200", m.group(4));
 		assertEquals("GET", m.group(5));
-		assertEquals("/?ad=google&gclid=EAIaIQobChMIzuS_-rqS-gIVwW3TCh13FQDiEAEYASAAEgIp1PD_BwE", m.group(6));
+		assertEquals("/js/main.js", m.group(6));
 		assertEquals("HTTP/1.1", m.group(7));
 		assertEquals("200", m.group(8));
-		assertEquals("6749", m.group(9));
-		assertEquals("https://abc.de", m.group(10));
+		assertEquals("4654", m.group(9));
+		assertEquals("https://blog.spontify.me/", m.group(10));
 		assertEquals(
-				"Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Mobile/15E148 Safari/604.1",
+				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.18 Safari/537.36",
 				m.group(11));
-		assertEquals("Tue Sep 13 21:04:54 CEST 2022", "" + dateFormat.parse(m.group(4)));
+		assertEquals("Sat Oct 22 06:58:54 CEST 2022", "" + dateFormat.parse(m.group(4)));
 	}
 
 	@Test
