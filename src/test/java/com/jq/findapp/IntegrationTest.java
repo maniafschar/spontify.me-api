5504ec11d8a2ba787f235b96b6fc12f4c119e139
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-01-26 15:40:40 +0100

    integration test event

--- src/test/java/com/jq/findapp/IntegrationTest.java
+++ src/test/java/com/jq/findapp/IntegrationTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.AfterAll;
@@ -15,7 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
@@ -35,6 +35,7 @@ import com.jq.findapp.util.Utils;
 public class IntegrationTest {
 	private static WebDriver driver;
 	private static String url = "http://localhost:9000/";
+	private static long sleep = 600;
 
 	@Autowired
 	private Utils utils;
@@ -43,6 +44,7 @@ public class IntegrationTest {
 	public static void start() throws Exception {
 		new ProcessBuilder("./web.sh start".split(" ")).start();
 		driver = new SafariDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@AfterAll
@@ -56,7 +58,10 @@ public class IntegrationTest {
 		init();
 		register("testabcd", "test@jq-consulting.de");
 		addLocation("location 1", "Melchiorstr. 9\n81479 München", false);
+		addEvent();
+		listMyEvents();
 		addLocation("location 1", "Melchiorstr. 9\n81479 München", true);
+		listLocations();
 		addFriend();
 	}
 
@@ -84,8 +89,7 @@ public class IntegrationTest {
 	private void addLocation(String name, String address, boolean duplicate) {
 		Util.click("navigation item.events");
 		Util.click("menu a[onclick*=\"pageEvent.edit\"]");
-		Util.sleep(500);
-		Util.click("buttontext[onclick*=\"pageLocation.edit\"]");
+		Util.click("popup buttontext[onclick*=\"pageLocation.edit\"]");
 		Util.sendKeys("popup input[name=\"name\"]", name);
 		Util.get("popup textarea[name=\"address\"]").clear();
 		Util.sendKeys("popup textarea[name=\"address\"]", address);
@@ -94,23 +98,59 @@ public class IntegrationTest {
 			Util.get("popup popupHint").getText().toLowerCase().contains("location");
 			Util.click("popupTitle");
 		}
-		Util.click("navigation item.home");
-		Util.sleep(300);
+	}
+
+	private void listLocations() {
+		Util.click("navigation item.search");
+		Util.click("search tabHeader tab[i=\"locations\"]");
+		Util.click("search tabBody div.locations buttontext[onclick*=\"pageSearch\"]");
+		if (!Util.get("search tabBody div.locations row:last-child div text title").getText().contains("location 1"))
+			throw new RuntimeException("New location not in list!");
+	}
+
+	private void addEvent() {
+		Util.click("navigation item.events");
+		Util.click("menu a[onclick*=\"pageEvent.edit\"]");
+		Util.click("popup eventLocationInputHelper li");
+		if (!Util.get("popup .paid").getAttribute("style").contains("none"))
+			throw new RuntimeException("Event .paid should be invisible!");
+		if (Util.get("popup .unpaid").getAttribute("style").contains("none"))
+			throw new RuntimeException("Event .unpaid should be visible!");
+		Util.sendKeys("popup input[name=\"price\"]", "10");
+		Util.sleep(sleep);
+		if (Util.get("popup .paid").getAttribute("style").contains("none"))
+			throw new RuntimeException("Event .paid should be visible!");
+		if (!Util.get("popup .unpaid").getAttribute("style").contains("none"))
+			throw new RuntimeException("Event .unpaid should be invisible!");
+		Util.get("popup input[name=\"price\"]").clear();
+		Util.click("popup hashtags category");
+		Util.click("popup dialogButtons buttontext[onclick*=\"save\"]");
+		Util.click("popup hashtags div label[onclick*=\"Architektur\"]");
+		Util.sendKeys("popup textarea[name=\"text\"]", "mega sex");
+		Util.click("popup dialogButtons buttontext[onclick*=\"save\"]");
+		Util.click("popup dialogButtons buttontext[onclick*=\"save\"]");
+	}
+
+	private void listMyEvents() {
+		Util.click("navigation item.events");
+		Util.click("menu a[onclick*=\"communication.loadList(ui.query.eventMy()\"]");
+		if (!Util.get("events row div text").getText().contains("mega ***"))
+			throw new RuntimeException("New event not in list!");
 	}
 
 	private void addFriend() {
 		Util.click("navigation item.search");
 		Util.click("search tabHeader tab[i=\"contacts\"]");
-		Util.sendKeys("search div.contacts input[name=\"keywords\"]", "pseudonym");
-		Util.click("search div.contacts buttontext[onclick*=\"pageSearch.\"]");
-		Util.click("search div.contacts row:nth-of-type(1)");
+		Util.sendKeys("search tabBody div.contacts input[name=\"keywords\"]", "pseudonym");
+		Util.click("search tabBody div.contacts buttontext[onclick*=\"pageSearch.\"]");
+		Util.click("search tabBody div.contacts row:nth-of-type(1)");
 		Util.click("detail buttontext[name=\"buttonFriend\"]");
 		Util.click("detail buttontext[onclick*=\"sendRequestForFriendship\"]");
 	}
 
 	private static class Util {
 		private static String email(int i) {
-			sleep(500);
+			sleep(sleep);
 			final List<String> files = Arrays.asList(new File("target/email").list());
 			files.sort((e1, e2) -> e1.compareTo(e2));
 			try {
@@ -131,28 +171,24 @@ public class IntegrationTest {
 		}
 
 		private static WebElement get(String id) {
-			for (int i = 0; i < 20; i++) {
-				try {
-					sleep(500);
-					return driver.findElement(By.cssSelector(id));
-				} catch (NoSuchElementException e) {
-					// wait
-				}
-			}
-			throw new RuntimeException(id + " not found");
+			WebElement d = driver.findElement(By.cssSelector(id));
+			System.out.println(d);
+			return d;
 		}
 
 		private static void click(String id) {
 			((JavascriptExecutor) driver).executeScript("arguments[0].click()", get(id));
+			Util.sleep(sleep);
 		}
 
 		private static void sendKeys(String id, String keys) {
 			for (int i = 0; i < 5; i++) {
 				try {
 					get(id).sendKeys(keys);
+					Util.sleep(sleep);
 					return;
-				} catch (NoSuchElementException | ElementNotInteractableException e) {
-					sleep(500);
+				} catch (ElementNotInteractableException e) {
+					sleep(sleep / 2);
 				}
 			}
 			throw new RuntimeException("Failed to send keys " + keys + " for id " + id);
