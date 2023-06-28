64af906c48e80d875811c373bd6870a3aa542eb2
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-06-28 16:33:25 +0200

    performance on import log

--- src/main/java/com/jq/findapp/service/backend/ImportLogService.java
+++ src/main/java/com/jq/findapp/service/backend/ImportLogService.java
@@ -10,7 +10,12 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,71 +36,90 @@ public class ImportLogService {
 	@Autowired
 	private IpService ipService;
 
+	private static final Map<String, Integer> linesRead = new HashMap<>();
+
 	public SchedulerResult importLog() {
 		final SchedulerResult result = new SchedulerResult(getClass().getSimpleName() + "/importLog");
 		try {
-			final String[] files = new File(".").list();
+			if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 1
+					&& Calendar.getInstance().get(Calendar.MINUTE) < 1)
+				linesRead.clear();
+			final List<String> files = Arrays.asList(new File(".").list());
+			files.sort((a, b) -> a.compareTo(b));
 			for (final String file : files) {
-				if (file.startsWith("log") && !file.contains(".") && !new File(file).isDirectory())
-					result.result += file + ": " + importLog(file) + "\n";
+				if (file.startsWith("log") && !file.contains(".") && !new File(file).isDirectory()) {
+					final int x = importLog(file);
+					if (x > 0)
+						result.result += x + " " + file + "\n";
+				}
 			}
-			result.result += "updated " + ipService.lookupLogIps() + " ips";
+			final long t = System.currentTimeMillis();
+			final int x = ipService.lookupLogIps();
+			result.result += x + " ips updated";
+			if (x > 0)
+				result.result += " in " + (System.currentTimeMillis() - t) + " ms";
 		} catch (final Exception e) {
 			result.exception = e;
 		}
 		return result;
 	}
 
-	private int importLog(final String name) throws Exception {
+	private int importLog(final String filename) throws Exception {
 		int count = 0;
 		final DateFormat dateParser = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.ENGLISH);
 		final Pattern pattern = Pattern.compile(
 				"([\\d.]+) (\\S) (\\S) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(\\w+) ([^ ]*) ([^\"]*)\" (\\d+) (\\d+) \"([^\"]*)\" \"([^\"]*)\"");
-		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(name)))) {
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
+			final int startFrom = linesRead.containsKey(filename) ? linesRead.get(filename) : 0;
 			final QueryParams params = new QueryParams(Query.misc_listLog);
-			final String uri = name.substring(3).replaceAll("\\d", "").toLowerCase();
+			final String uri = filename.substring(3).replaceAll("\\d", "").toLowerCase();
 			String line;
+			int lines = 0;
 			while ((line = reader.readLine()) != null) {
-				final Matcher m = pattern.matcher(line.replaceAll("\\\\\"", ""));
-				if (m.find()) {
-					final String path = m.group(6);
-					final Log log = new Log();
-					log.setIp(IpService.sanatizeIp(m.group(1)));
-					log.setMethod(m.group(5));
-					log.setStatus(Integer.parseInt(m.group(8)));
-					if (!"-".equals(m.group(10))) {
-						log.setReferer(m.group(10));
-						if (log.getReferer().length() > 255)
-							log.setReferer(log.getReferer().substring(0, 255));
-					}
-					log.setCreatedAt(new Timestamp(dateParser.parse(m.group(4)).getTime()));
-					log.setBody(m.group(11).replace("&#39;", "'"));
-					if (log.getBody().length() > 255)
-						log.setBody(log.getBody().substring(0, 255));
-					log.setPort(80);
-					if (log.getStatus() < 400 &&
-							((path.endsWith(".html") && !path.startsWith("/js/")) || path.endsWith(".pdf") ||
-									(!path.contains(".")
-											&& !path.contains("apple-app-site-association")
-											&& !path.startsWith("/rest/")))) {
-						log.setUri(uri);
-						if (path.length() > 2) {
-							final String[] s = path.split("\\?");
-							if (s[0].length() > 1)
-								log.setUri(log.getUri() + URLDecoder.decode(s[0], StandardCharsets.UTF_8));
-							if (s.length > 1)
-								log.setQuery(s[1]);
-						}
-						params.setSearch("log.ip='" + log.getIp() + "' and log.uri='" + log.getUri()
-								+ "' and log.createdAt='" + Instant.ofEpochMilli(log.getCreatedAt().getTime())
-								+ "' and log.body like '" + log.getBody().replace("'", "''") + "'");
-						if (repository.list(params).size() == 0) {
-							repository.save(log);
-							count++;
+				if (++lines > startFrom) {
+					final Matcher m = pattern.matcher(line.replaceAll("\\\\\"", ""));
+					if (m.find()) {
+						final String path = m.group(6);
+						final Log log = new Log();
+						log.setStatus(Integer.parseInt(m.group(8)));
+						if (log.getStatus() < 400 &&
+								((path.endsWith(".html") && !path.startsWith("/js/")) || path.endsWith(".pdf") ||
+										(!path.contains(".")
+												&& !path.contains("apple-app-site-association")
+												&& !path.startsWith("/rest/")))) {
+							log.setCreatedAt(new Timestamp(dateParser.parse(m.group(4)).getTime()));
+							log.setIp(IpService.sanatizeIp(m.group(1)));
+
+							log.setBody(m.group(11).replace("&#39;", "'"));
+							if (log.getBody().length() > 255)
+								log.setBody(log.getBody().substring(0, 255));
+							log.setUri(uri);
+							if (path.length() > 2) {
+								final String[] s = path.split("\\?");
+								if (s[0].length() > 1)
+									log.setUri(log.getUri() + URLDecoder.decode(s[0], StandardCharsets.UTF_8));
+								if (s.length > 1)
+									log.setQuery(s[1]);
+							}
+							params.setSearch("log.ip='" + log.getIp() + "' and log.uri='" + log.getUri()
+									+ "' and log.createdAt='" + Instant.ofEpochMilli(log.getCreatedAt().getTime())
+									+ "' and log.body like '" + log.getBody().replace("'", "''") + "'");
+							if (repository.list(params).size() == 0) {
+								log.setPort(80);
+								log.setMethod(m.group(5));
+								if (!"-".equals(m.group(10))) {
+									log.setReferer(m.group(10));
+									if (log.getReferer().length() > 255)
+										log.setReferer(log.getReferer().substring(0, 255));
+								}
+								repository.save(log);
+								count++;
+							}
 						}
 					}
 				}
 			}
+			linesRead.put(filename, lines);
 		}
 		return count;
 	}
