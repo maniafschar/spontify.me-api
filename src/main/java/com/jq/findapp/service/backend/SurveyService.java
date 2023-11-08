eaa1fdc66bf0268cfc0577a4b57d7053abebc74f
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-11-08 08:33:37 +0100

    Update SurveyService.java

--- src/main/java/com/jq/findapp/service/backend/SurveyService.java
+++ src/main/java/com/jq/findapp/service/backend/SurveyService.java
@@ -66,256 +66,257 @@ public class SurveyService {
 
 		}
 	}
+}
 
-	class MatchDay {
-		public Fixture fixture;
-		public League league;
-		public Teams teams;
-		public Goals goals;
-		public Score score;
-		public ArrayList<Event> events;
-		public ArrayList<Lineup> lineups;
-		public ArrayList<Statistic> statistics;
-		public ArrayList<Player> players;
-	}
+class MatchDay {
+	public Fixture fixture;
+	public League league;
+	public Teams teams;
+	public Goals goals;
+	public Score score;
+	public ArrayList<Event> events;
+	public ArrayList<Lineup> lineups;
+	public ArrayList<Statistic> statistics;
+	public ArrayList<Player> players;
+}
 
-	class Assist {
-		public int id;
-		public String name;
-	}
+class Assist {
+	public int id;
+	public String name;
+}
 
-	class Away {
-		public int id;
-		public String name;
-		public String logo;
-		public boolean winner;
-	}
+class Away {
+	public int id;
+	public String name;
+	public String logo;
+	public boolean winner;
+}
 
-	class Cards {
-		public int yellow;
-		public int red;
-	}
+class Cards {
+	public int yellow;
+	public int red;
+}
 
-	class Coach {
-		public int id;
-		public String name;
-		public String photo;
-	}
+class Coach {
+	public int id;
+	public String name;
+	public String photo;
+}
 
-	class Colors {
-		public Player player;
-		public Goalkeeper goalkeeper;
-	}
+class Colors {
+	public Player player;
+	public Goalkeeper goalkeeper;
+}
 
-	class Dribbles {
-		public int attempts;
-		public int success;
-		public int past;
-	}
+class Dribbles {
+	public int attempts;
+	public int success;
+	public int past;
+}
 
-	class Duels {
-		public int total;
-		public int won;
-	}
+class Duels {
+	public int total;
+	public int won;
+}
 
-	class Event {
-		public Time time;
-		public Team team;
-		public Player player;
-		public Assist assist;
-		public String type;
-		public String detail;
-		public String comments;
-	}
+class Event {
+	public Time time;
+	public Team team;
+	public Player player;
+	public Assist assist;
+	public String type;
+	public String detail;
+	public String comments;
+}
 
-	class Extratime {
-		public Object home;
-		public Object away;
-	}
+class Extratime {
+	public Object home;
+	public Object away;
+}
 
-	class Fixture {
-		public int id;
-		public String referee;
-		public String timezone;
-		public Date date;
-		public int timestamp;
-		public Periods periods;
-		public Venue venue;
-		public Status status;
-	}
+class Fixture {
+	public int id;
+	public String referee;
+	public String timezone;
+	public Date date;
+	public int timestamp;
+	public Periods periods;
+	public Venue venue;
+	public Status status;
+}
 
-	class Fouls {
-		public int drawn;
-		public int committed;
-	}
+class Fouls {
+	public int drawn;
+	public int committed;
+}
 
-	class Fulltime {
-		public int home;
-		public int away;
-	}
+class Fulltime {
+	public int home;
+	public int away;
+}
 
-	class Games {
-		public int minutes;
-		public int number;
-		public String position;
-		public String rating;
-		public boolean captain;
-		public boolean substitute;
-	}
+class Games {
+	public int minutes;
+	public int number;
+	public String position;
+	public String rating;
+	public boolean captain;
+	public boolean substitute;
+}
 
-	class Goalkeeper {
-		public String primary;
-		public String number;
-		public String border;
-	}
+class Goalkeeper {
+	public String primary;
+	public String number;
+	public String border;
+}
 
-	class Goals {
-		public int home;
-		public int away;
-		public int total;
-		public int conceded;
-		public int assists;
-		public int saves;
-	}
+class Goals {
+	public int home;
+	public int away;
+	public int total;
+	public int conceded;
+	public int assists;
+	public int saves;
+}
 
-	class Halftime {
-		public int home;
-		public int away;
-	}
+class Halftime {
+	public int home;
+	public int away;
+}
 
-	class Home {
-		public int id;
-		public String name;
-		public String logo;
-		public boolean winner;
-	}
+class Home {
+	public int id;
+	public String name;
+	public String logo;
+	public boolean winner;
+}
 
-	class League {
-		public int id;
-		public String name;
-		public String country;
-		public String logo;
-		public String flag;
-		public int season;
-		public String round;
-	}
+class League {
+	public int id;
+	public String name;
+	public String country;
+	public String logo;
+	public String flag;
+	public int season;
+	public String round;
+}
 
-	class Lineup {
-		public Team team;
-		public Coach coach;
-		public String formation;
-		public ArrayList<StartXI> startXI;
-		public ArrayList<Substitute> substitutes;
-	}
+class Lineup {
+	public Team team;
+	public Coach coach;
+	public String formation;
+	public ArrayList<StartXI> startXI;
+	public ArrayList<Substitute> substitutes;
+}
 
-	class Passes {
-		public int total;
-		public int key;
-		public String accuracy;
-	}
+class Passes {
+	public int total;
+	public int key;
+	public String accuracy;
+}
 
-	class Penalty {
-		public Object home;
-		public Object away;
-		public Object won;
-		public Object commited;
-		public int scored;
-		public int missed;
-		public int saved;
-	}
+class Penalty {
+	public Object home;
+	public Object away;
+	public Object won;
+	public Object commited;
+	public int scored;
+	public int missed;
+	public int saved;
+}
 
-	class Periods {
-		public int first;
-		public int second;
-	}
+class Periods {
+	public int first;
+	public int second;
+}
 
-	class Player {
-		public int id;
-		public String name;
-		public String primary;
-		public String number;
-		public String border;
-		public String pos;
-		public String grid;
-		public String photo;
-	}
+class Player {
+	public int id;
+	public String name;
+	public String primary;
+	public String number;
+	public String border;
+	public String pos;
+	public String grid;
+	public String photo;
+}
 
-	class Player5 {
-		public Team team;
-		public ArrayList<Player> players;
-		public Player player;
-		public ArrayList<Statistic> statistics;
-	}
+class Player5 {
+	public Team team;
+	public ArrayList<Player> players;
+	public Player player;
+	public ArrayList<Statistic> statistics;
+}
 
-	class Score {
-		public Halftime halftime;
-		public Fulltime fulltime;
-		public Extratime extratime;
-		public Penalty penalty;
-	}
+class Score {
+	public Halftime halftime;
+	public Fulltime fulltime;
+	public Extratime extratime;
+	public Penalty penalty;
+}
 
-	class Shots {
-		public int total;
-		public int on;
-	}
+class Shots {
+	public int total;
+	public int on;
+}
 
-	class StartXI {
-		public Player player;
-	}
+class StartXI {
+	public Player player;
+}
 
-	class Statistic {
-		public Team team;
-		public ArrayList<Statistic> statistics;
-		public String type;
-		public Object value;
-		public Games games;
-		public int offsides;
-		public Shots shots;
-		public Goals goals;
-		public Passes passes;
-		public Tackles tackles;
-		public Duels duels;
-		public Dribbles dribbles;
-		public Fouls fouls;
-		public Cards cards;
-		public Penalty penalty;
-	}
+class Statistic {
+	public Team team;
+	public ArrayList<Statistic> statistics;
+	public String type;
+	public Object value;
+	public Games games;
+	public int offsides;
+	public Shots shots;
+	public Goals goals;
+	public Passes passes;
+	public Tackles tackles;
+	public Duels duels;
+	public Dribbles dribbles;
+	public Fouls fouls;
+	public Cards cards;
+	public Penalty penalty;
+}
 
-	class Status {
-		@JsonProperty("long")
-		public String mylong;
-		@JsonProperty("short")
-		public String myshort;
-		public int elapsed;
-	}
+class Status {
+	@JsonProperty("long")
+	public String mylong;
+	@JsonProperty("short")
+	public String myshort;
+	public int elapsed;
+}
 
-	class Substitute {
-		public Player player;
-	}
+class Substitute {
+	public Player player;
+}
 
-	class Tackles {
-		public int total;
-		public int blocks;
-		public int interceptions;
-	}
+class Tackles {
+	public int total;
+	public int blocks;
+	public int interceptions;
+}
 
-	class Team {
-		public int id;
-		public String name;
-		public String logo;
-		public Colors colors;
-		public Date update;
-	}
+class Team {
+	public int id;
+	public String name;
+	public String logo;
+	public Colors colors;
+	public Date update;
+}
 
-	class Teams {
-		public Home home;
-		public Away away;
-	}
+class Teams {
+	public Home home;
+	public Away away;
+}
 
-	class Time {
-		public int elapsed;
-		public int extra;
-	}
+class Time {
+	public int elapsed;
+	public int extra;
+}
 
 class Venue {
 	public int id;
