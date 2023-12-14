af8c8443f16dbfca4ff16af64e180881b591ef24
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-12-14 10:13:58 +0100

    Create ImportHamburg.java

---
+++ src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
@@ -0,0 +1,9 @@
+/*
+https://www.hamburg.de/tickets/
+https://www.berlin.de/tickets/suche/
+https://frankfurt.de/veranstaltungen
+https://termine.koeln.de/koeln
+https://www.stuttgart.de/service/veranstaltungen.php
+https://veranstaltungen.freiburg.de/events/
+https://www.leipzig.de/freizeit-kultur-und-tourismus/veranstaltungen-und-termine
+*/
