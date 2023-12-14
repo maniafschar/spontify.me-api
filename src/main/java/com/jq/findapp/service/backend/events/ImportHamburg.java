bdcb6e50e3dd1b94d063d83eb29424eb73071fcc
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-12-14 15:50:49 +0100

    Update ImportHamburg.java

--- src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
+++ src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
@@ -11,4 +11,19 @@ https://feed.rundschau-online.de/feed/rss/region/index.rss Köln
 https://www.rbb24.de/aktuell/index.xml/feed=rss.xml Berlin
 http://www.ndr.de/home/index-rss.xml
 https://www.hessenschau.de/rheinmain/index.rss
+https://rss.sueddeutsche.de/rss/Muenchen
+https://www.leipzig.de/newsfeed/rss.xml
+
+<item>
+  <guid isPermaLink="false">news-48431</guid>
+  <pubDate>Thu, 14 Dec 2023 12:29:11 +0100</pubDate>
+  <title>Stadtrat gibt für Liniennetz der Zukunft grünes Licht</title>
+  <link>https://www.leipzig.de/news/news/stadtrat-gibt-fuer-liniennetz-der-zukunft-gruenes-licht</link>
+  <description>Der Leipziger Stadtrat hat in seiner Sitzung am 13. Dezember 2023 unter anderem seine Zustimmung gemäß des Nahverkehrsplans, für das Liniennetz der Zukunft, erteilt.</description>
+  <enclosure url="https://www.leipzig.de/fileadmin/_processed_/5/c/csm_Mobilitatsszenarien_2030_4cf61abde1.jpg" length="117735" type="image/jpeg"/>
+  <content:encoded>
+  <![CDATA[ <p> </p><p>Für Kundinnen und Kunden bedeutet dies zahlreiche attraktive Angebotsveränderungen bei Bus und Bahn, welche mit dem Liniennetz der Zukunft umgesetzt werden sollen. Dazu zählt unter anderem auch, dass sich der Pilot-Zeitraum für die Flexa-Gebiete Südwest und Leutzsch bis Dezember 2026 verlängern…</p> ]]>
+  </content:encoded>
+  <enclosure url="https://www.leipzig.de/fileadmin/_processed_/5/c/csm_Mobilitatsszenarien_2030_fcfdea3655.jpg" length="117735" type="image/jpeg"/>
+</item>
 */
