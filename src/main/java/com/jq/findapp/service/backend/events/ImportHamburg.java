4c19df02e115b2cc79792264212593feb8263503
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-12-14 11:32:03 +0100

    Update ImportHamburg.java

--- src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
+++ src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
@@ -6,4 +6,9 @@ https://termine.koeln.de/koeln
 https://www.stuttgart.de/service/veranstaltungen.php
 https://veranstaltungen.freiburg.de/events/
 https://www.leipzig.de/freizeit-kultur-und-tourismus/veranstaltungen-und-termine
+
+https://feed.rundschau-online.de/feed/rss/region/index.rss Köln
+https://www.rbb24.de/aktuell/index.xml/feed=rss.xml Berlin
+http://www.ndr.de/home/index-rss.xml
+https://www.hessenschau.de/rheinmain/index.rss
 */
