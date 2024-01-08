44c655ebf695624cf03f566068a7d74ee4008147
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-01-08 09:23:42 +0100

    Update ActionApiTest.java

--- src/test/java/com/jq/findapp/api/ActionApiTest.java
+++ src/test/java/com/jq/findapp/api/ActionApiTest.java
@@ -63,29 +63,4 @@ public class ActionApiTest {
 		// then
 		assertEquals("https://api.sandbox.paypal.com/v2/payments/captures/2J476521SE713422J", href);
 	}
-
-	@Test
-	public void replaceFirst() {
-		// given
-		final String s = "<htmtl>\n\t<body>\n\t\t<meta property=\"og:url\" content=\"aaa\"/>\n\t</body>\n</html>";
-
-		// when
-		final String result = s.replaceFirst("<meta property=\"og:url\" content=\"([^\"].*)\"",
-				"<meta property=\"og:url\" content=\"xxx\"");
-
-		// then
-		assertEquals("<htmtl>\n\t<body>\n\t\t<meta property=\"og:url\" content=\"xxx\"/>\n\t</body>\n</html>", result);
-	}
-
-	@Test
-	public void replace_alternate() {
-		// given
-		final String s = "<htmtl>\n\t<body>\n\t\t<link rel=\"alternate\" href=\"aaa\"/>\n\t</body>\n</html>";
-
-		// when
-		final String result = s.replaceFirst("(<link rel=\"alternate\" ([^>].*)>)", "");
-
-		// then
-		assertEquals("<htmtl>\n\t<body>\n\t\t\n\t</body>\n</html>", result);
-	}
 }
