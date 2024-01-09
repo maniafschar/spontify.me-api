d1f7ae4575c0e57f6bee44d9fe9fdab2c7813090
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-01-09 10:40:57 +0100

    Update FindappApplication.java

--- src/main/java/com/jq/findapp/FindappApplication.java
+++ src/main/java/com/jq/findapp/FindappApplication.java
@@ -1,9 +1,12 @@
 package com.jq.findapp;
 
+import com.twelvemonkeys.servlet.image.IIOProviderContextListener;
+
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -11,13 +14,19 @@ import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableAsync
-public class FindappApplication {
+public class FindappApplication extends SpringBootServletInitializer {
 
 	public static void main(final String[] args) {
 		final SpringApplicationBuilder app = new SpringApplicationBuilder(FindappApplication.class);
 		app.application().addListeners(new ApplicationPidFileWriter(
 				"./shutdown" + System.getProperties().getProperty("server.port") + ".pid"));
 		app.run(args);
+	}
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		servletContext.addListener(IIOProviderContextListener.class);
 	}
 
 	@Bean
