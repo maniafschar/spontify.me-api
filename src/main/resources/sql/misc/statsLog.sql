deb9de487c3bc9a4970bc4bbbaf7f8a55e9a50b8
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-01-11 20:32:50 +0100

    log stats

--- src/main/resources/sql/misc/statsLog.sql
+++ src/main/resources/sql/misc/statsLog.sql
@@ -1,5 +1,5 @@
 SELECT
-	case when (cast(log.time as integer)-5+0.5)/10>20 then 20 else (cast(log.time as integer)-5+0.5)/10 end as time,
+	case when cast((cast(log.time as integer)-5+0.5)/10 as integer)>20 then 20 else cast((cast(log.time as integer)-5+0.5)/10 as integer) end as time,
 	count(*)*1.0/(select count(*) from Log where clientId={CLIENTID} and uri like '/%' and uri not like '/support/%' and uri not like '/ws/%' and cast(TO_DAYS(createdAt) as integer)>cast(TO_DAYS(current_timestamp) as integer)-90) as count
 FROM
 	Log log
