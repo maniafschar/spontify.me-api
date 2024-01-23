314aac16d8f72ce6db19b3794debc2bbd6489432
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-01-23 15:34:48 +0100

    Update ImportHamburg.java

--- src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
+++ src/main/java/com/jq/findapp/service/backend/events/ImportHamburg.java
@@ -7,6 +7,14 @@
  * https://veranstaltungen.freiburg.de/events/
  * https://www.leipzig.de/freizeit-kultur-und-tourismus/veranstaltungen-und-termine
  * 
+
+Liebe FC Bayern Total Fans,
+lieben Dank dass Ihr Euch in unserer FC Bayern Total Fanclub-App angemeldet und Teil unseres Netzwerks geworden seid.
+Wir möchten diese App nun weiterführen und für alle Fußball-Fans öffnen. Dabei hat jeder Fan die Möglichkeit, einen oder mehrere Lieblingsclubs auszuwählen.
+Ihr werdet dann auch nur andere Fans Deiner Lieblingsvereine und deren Events/News sehen. Somit werden wir sowohl das Logo, als auch den Namen der App neu vergeben und neutral halten.
+Umfragen vor und nach den Spielen wird es weiterhin geben und wir freuen uns über jede weiter Anregung.
+Viele Grüße
+
  * <item>
  * <guid isPermaLink="false">news-48431</guid>
  * <pubDate>Thu, 14 Dec 2023 12:29:11 +0100</pubDate>
