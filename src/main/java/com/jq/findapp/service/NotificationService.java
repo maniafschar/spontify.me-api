3931b9950c8d26451096ebb04258eb46ad7a1351
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-05-03 09:30:24 +0200

    Update NotificationService.java

--- src/main/java/com/jq/findapp/service/NotificationService.java
+++ src/main/java/com/jq/findapp/service/NotificationService.java
@@ -454,7 +454,7 @@ public class NotificationService {
 
 	public void createTicket(final TicketType type, String subject, String text, final BigInteger contactId) {
 		try {
-			if (type == TicketType.ERROR && "client".equals(subject)) {
+			if (type == TicketType.ERROR) {
 				final QueryParams params = new QueryParams(Query.misc_listTicket);
 				params.setSearch("storage.label='logErrorExclusionRegex'");
 				final Map<String, Object> exclude = repository.one(params);
