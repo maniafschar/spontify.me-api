4ae204c3f14af15a54b7e85dceb561d136b62b32
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-05-22 08:37:52 +0200

    Update ImportLocationsService.java

--- src/main/java/com/jq/findapp/service/backend/ImportLocationsService.java
+++ src/main/java/com/jq/findapp/service/backend/ImportLocationsService.java
@@ -292,31 +292,37 @@ public class ImportLocationsService {
 							externalService
 									.google("place/textsearch/json?query=" + URLEncoder.encode(location.getName() + ","
 											+ location.getAddress().replaceAll("\n", ","), StandardCharsets.UTF_8)));
-					if ("OK".equals(json.get("status").asText()) && json.get("results").get(0).has("photos")) {
+					if ("OK".equals(json.get("status").asText())) {
 						for (int i2 = 0; i2 < json.get("results").size(); i2++) {
-							final String html = externalService.google(
-									"place/photo?maxheight=1200&photoreference="
-											+ json.get("results").get(i2).get("photos").get(0).get("photo_reference")
-													.asText())
-									.replace("<A HREF=", "<a href=");
-							final Matcher matcher = href.matcher(html);
-							if (matcher.find()) {
-								location.historize();
-								final String image = matcher.group(1);
-								try {
-									location.setImage(EntityUtil.getImage(image, EntityUtil.IMAGE_SIZE, 0));
-									location.setImageList(EntityUtil.getImage(image, EntityUtil.IMAGE_THUMB_SIZE, 0));
-									repository.save(location);
-									updated++;
-									break;
-								} catch (IllegalArgumentException ex) {
-									if (!ex.getMessage().contains("IMAGE_TOO_SMALL"))
-										notificationService.createTicket(TicketType.ERROR, "importImage",
-												Strings.stackTraceToString(ex), null);
+							if (json.get("results").get(i2).has("photos") &&
+							    	json.get("results").get(i2).get("photos").get(0).has("photo_reference")) {
+								final String html = externalService.google(
+										"place/photo?maxheight=1200&photoreference="
+												+ json.get("results").get(i2).get("photos").get(0).get("photo_reference")
+														.asText())
+										.replace("<A HREF=", "<a href=");
+								final Matcher matcher = href.matcher(html);
+								if (matcher.find()) {
+									location.historize();
+									final String image = matcher.group(1);
+									try {
+										location.setImage(EntityUtil.getImage(image, EntityUtil.IMAGE_SIZE, 0));
+										location.setImageList(EntityUtil.getImage(image, EntityUtil.IMAGE_THUMB_SIZE, 0));
+										repository.save(location);
+										updated++;
+										break;
+									} catch (IllegalArgumentException ex) {
+										if (!ex.getMessage().contains("IMAGE_TOO_SMALL"))
+											notificationService.createTicket(TicketType.ERROR, "importImage",
+													Strings.stackTraceToString(ex), null);
+									}
 								}
 							}
 						}
-					}
+					} else
+						notificationService.createTicket(TicketType.ERROR, "importImageNotFound", location.getName() + ","
+								+ location.getAddress().replaceAll("\n", ",") + "\n"
+								+ new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(json), null);
 				}
 			} catch (Exception ex) {
 				result.exception = ex;
