27750fd72e6b4a06847b070d7fe24046ee3b3bd3
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-07-11 09:49:15 +0200

    Update and rename LocationTest.java to Location.java

--- src/test/java/com/jq/findapp/it/Location.java
+++ src/test/java/com/jq/findapp/it/Location.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocationTest {
+public class Location {
 	private WebDriver driver;
 
 	@BeforeEach
