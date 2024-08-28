fdce1496d033d8849961ffda37966ed4dc460a88
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-08-28 14:47:15 +0200

    Update SupportCenterApi.java

--- src/main/java/com/jq/findapp/api/SupportCenterApi.java
+++ src/main/java/com/jq/findapp/api/SupportCenterApi.java
@@ -380,6 +380,8 @@ public class SupportCenterApi {
 			run(chatService, null, list, null, -1);
 			run(marketingService, "Sportbars", list,
 					new int[] { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }, -1);
+			run(marketingService, "Unfinished", list,
+					new int[] { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }, -1);
 			run(dbService, null, list, null, -1);
 			run(dbService, "LogFix", list, null, -1);
 			run(dbService, "CleanUp", list, new int[] { 0 }, 30);
