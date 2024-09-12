173fee5a275a246f0a0d8d5bcd04278230ec00b7
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-09-12 08:31:43 +0200

    Update SitemapService.java

--- src/main/java/com/jq/findapp/service/backend/SitemapService.java
+++ src/main/java/com/jq/findapp/service/backend/SitemapService.java
@@ -26,10 +26,9 @@ public class SitemapService {
 
 	public SchedulerResult run() {
 		final SchedulerResult result = new SchedulerResult();
-		final Result list = repository.list(new QueryParams(Query.misc_listClient));
-		for (int i = 0; i < list.size(); i++) {
+		repository.list(new QueryParams(Query.misc_listClient)).forEach(e -> {
 			try {
-				final JsonNode json = new ObjectMapper().readTree(list.get(i).get("client.storage").toString())
+				final JsonNode json = new ObjectMapper().readTree(e.get("client.storage").toString())
 						.get("sitemap");
 				if (json != null) {
 					final StringBuilder sitemap = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
@@ -37,47 +36,45 @@ public class SitemapService {
 					if (json.get("type").asText().contains("event")) {
 						final QueryParams params = new QueryParams(Query.event_listId);
 						params.setSearch(
-								"event.contactId=" + list.get(i).get("client.adminId")
+								"event.contactId=" + e.get("client.adminId")
 										+ " and event.endDate>cast('"
 										+ Instant.now() + "' as timestamp)");
-						writeMap(json, "event", params, sitemap, (String) list.get(i).get("client.url"));
+						writeMap(json, "event", params, sitemap, (String) e.get("client.url"));
 					}
 					if (json.get("type").asText().contains("news")) {
 						final QueryParams params = new QueryParams(Query.misc_listNews);
 						params.setUser(new Contact());
-						params.getUser().setClientId((BigInteger) list.get(i).get("client.id"));
+						params.getUser().setClientId((BigInteger) e.get("client.id"));
 						params.setSearch("clientNews.publish<cast('" + Instant.now() + "' as timestamp)");
-						writeMap(json, "news", params, sitemap, (String) list.get(i).get("client.url"));
+						writeMap(json, "news", params, sitemap, (String) e.get("client.url"));
 					}
 					if (json.get("type").asText().contains("location"))
 						writeMap(json, "location", new QueryParams(Query.location_listId), sitemap,
-								(String) list.get(i).get("client.url"));
+								(String) e.get("client.url"));
 					sitemap.append("</sitemapindex>");
 					IOUtils.write(sitemap.toString().getBytes(StandardCharsets.UTF_8),
 							new FileOutputStream(json.get("path").asText() + File.separatorChar + "sitemap.xml"));
-					result.body += "updated " + list.get(i).get("client.id");
+					result.body += "updated " + e.get("client.id");
 				}
 			} catch (final Exception e) {
-				result.body += list.get(i).get("client.id") + ", error " + e.getMessage() + "\n";
+				result.body += e.get("client.id") + ", error " + e.getMessage() + "\n";
 				if (result.exception == null)
 					result.exception = e;
 			}
-		}
+		});
 		return result;
 	}
 
 	private void writeMap(final JsonNode json, final String type, final QueryParams params,
 			final StringBuilder sitemap, final String url) throws Exception {
 		params.setLimit(0);
-		final Result list = repository.list(params);
 		final String urlList = url + "/rest/marketing/" + type + "/";
 		final String name = "sitemap_" + type + ".txt";
 		final String filename = json.get("path").asText() + File.separatorChar + name;
 		new File(filename).delete();
 		try (final FileOutputStream out = new FileOutputStream(filename)) {
-			for (int i = 0; i < list.size(); i++)
-				out.write((urlList + list.get(i).get(("news".equals(type) ? "clientNews" : type) + ".id") + "\n")
-						.getBytes(StandardCharsets.UTF_8));
+			repository.list(params).forEach(e -> out.write((urlList + e.get(("news".equals(type) ? "clientNews" : type) + ".id") + "\n")
+					.getBytes(StandardCharsets.UTF_8)));
 		}
 		sitemap.append("<sitemap><loc>" + url + "/" + name + "</loc></sitemap>");
 	}
