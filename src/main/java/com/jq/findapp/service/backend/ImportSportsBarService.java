7e084cef195448a5df321a219653d568c3c15f56
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-09-22 18:30:26 +0200

    dazn

--- src/main/java/com/jq/findapp/service/backend/ImportSportsBarService.java
+++ src/main/java/com/jq/findapp/service/backend/ImportSportsBarService.java
@@ -157,7 +157,9 @@ public class ImportSportsBarService {
 									loc.setSkills(Strings.isEmpty(loc.getSkills()) ? "x.1" : loc.getSkills() + "|x.1");
 									repository.save(loc);
 								}
-							} else
+							} else if (ex.getMessage().contains("OVER_QUERY_LIMIT"))
+								throw ex;
+							else
 								notificationService.createTicket(TicketType.ERROR, "importSportBar",
 										Strings.stackTraceToString(ex), BigInteger.ONE);
 						}
