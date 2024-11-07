99e94ba937ea554c28e5eacbb9ac6cdac8861b13
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-07 13:00:45 +0100

    Update ActionApi.java

--- src/main/java/com/jq/findapp/api/ActionApi.java
+++ src/main/java/com/jq/findapp/api/ActionApi.java
@@ -284,7 +284,7 @@ public class ActionApi {
 		final List<Object[]> list = repository.list(params).getList();
 		if (user != null && clientId.intValue() == 4) {
 			final Contact contact = repository.one(Contact.class, user);
-			final String s = matchDayService.retrieveMatchDays(2, 4, Arrays.asList(contact.getSkills().split("|"))
+			final String s = matchDayService.retrieveMatchDays(2, 4, Arrays.asList(contact.getSkills().split("\\|"))
 					.stream().filter(e -> e.startsWith("9.")).map(e -> Integer.valueOf(e.substring(2))).collect(Collectors.toList()));
 			if (!Strings.isEmpty(s)) {
 				final Object[] o = new Object[list.get(0).length];
