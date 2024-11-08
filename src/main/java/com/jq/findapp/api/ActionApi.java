42239fbac4d60aad81562c381966267862054c1a
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-08 11:36:57 +0100

    Update ActionApi.java

--- src/main/java/com/jq/findapp/api/ActionApi.java
+++ src/main/java/com/jq/findapp/api/ActionApi.java
@@ -9,7 +9,6 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
