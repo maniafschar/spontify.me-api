9d02c4df2e6875157d7d47f29f9bc015f46f4d27
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-15 16:16:03 +0100

    Update MarketingLocationService.java

--- src/main/java/com/jq/findapp/service/backend/MarketingLocationService.java
+++ src/main/java/com/jq/findapp/service/backend/MarketingLocationService.java
@@ -39,6 +39,7 @@ import com.jq.findapp.repository.Repository.Attachment;
 import com.jq.findapp.service.AuthenticationService;
 import com.jq.findapp.service.NotificationService;
 import com.jq.findapp.service.backend.CronService.CronResult;
+import com.jq.findapp.service.backend.CronService.Job;
 import com.jq.findapp.util.Json;
 import com.jq.findapp.util.Strings;
 import com.jq.findapp.util.Text;
@@ -68,6 +69,7 @@ public class MarketingLocationService {
 		public String apply(X x, Y y, Z z);
 	}
 
+	@Job(cron = "* 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21")
 	public CronResult run() {
 		final CronResult result = new CronResult();
 		final QueryParams params = new QueryParams(Query.misc_listMarketing);
@@ -148,6 +150,7 @@ public class MarketingLocationService {
 		return result;
 	}
 
+	@Job(cron = "30 17")
 	public CronResult runUnfinished() {
 		return sendEmails("contactMarketing.finished=false and contactMarketing.createdAt>cast('"
 				+ Instant.now().minus(Duration.ofDays(14)).toString()
@@ -160,6 +163,7 @@ public class MarketingLocationService {
 						+ location.getId() + "&h=" + location.getSecret().hashCode());
 	}
 
+	@Job(cron = "10 19")
 	public CronResult runSent() {
 		return sendEmails(
 				"contactMarketing.createdAt<cast('" + Instant.parse("2024-09-04T05:00:00.00Z").toString()
@@ -180,6 +184,7 @@ public class MarketingLocationService {
 						: "/?i=" + location.getId() + "&h=" + location.getSecret().hashCode()));
 	}
 
+	@Job(cron = "40 17")
 	public CronResult runCooperation() {
 		return sendEmails(
 				"1=1",
