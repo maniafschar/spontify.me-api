12ddf3d3215f2950ceab0442b3ed75c68275bce9
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-21 07:58:35 +0100

    marketing.runEvent

--- src/main/java/com/jq/findapp/service/MarketingService.java
+++ src/main/java/com/jq/findapp/service/MarketingService.java
@@ -64,7 +64,7 @@ public class MarketingService {
 			if (node.has("marketing")) {
 				int count = 0;
 				for (int i2 = 0; i2 < node.get("marketing").size(); i2++) {
-					if (Math.random() > 0.8) {
+					if (Math.random() > 0.9) {
 						final long contactId = node.get("marketing").get(i2).get("user").asLong();
 						params.setSearch(
 								"event.contactId=" + contactId + " and event.type='Inquiry' and event.startDate>=cast('"
