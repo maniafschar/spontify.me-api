33f2bcad18daf2469b232791fa2c3e64c1b4768d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-29 09:50:36 +0100

    Update ClientNewsService.java

--- src/main/java/com/jq/findapp/service/ClientNewsService.java
+++ src/main/java/com/jq/findapp/service/ClientNewsService.java
@@ -3,6 +3,7 @@ package com.jq.findapp.service;
 import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,35 +28,27 @@ public class ClientNewsService {
 	@Autowired
 	private NotificationService notificationService;
 
-	@Async
-	public void notify(ClientNews clientNews) {
+	public void notify(final ClientNews clientNews) {
 		if (clientNews.getPublish() != null
-				&& !clientNews.getPublish().after(new Timestamp(Instant.now().toEpochMilli()))
 				&& (clientNews.getNotified() == null || !clientNews.getNotified())
-				&& !Strings.isEmpty(clientNews.getSkills())) {
-			try {
-				clientNews.setNotified(true);
-				repository.save(clientNews);
+				&& !Strings.isEmpty(clientNews.getSkills())
+		    		&& !clientNews.getPublish().after(new Timestamp(Instant.now().toEpochMilli()))) {
+			clientNews.setNotified(true);
+			repository.save(clientNews);
+			CompletableFuture.supplyAsync(() -> {
 				final QueryParams params = new QueryParams(Query.contact_listId);
 				params.setSearch(
 						"contact.clientId=" + clientNews.getClientId()
 								+ " and contact.verified=true and cast(REGEXP_LIKE(contact.skills, '"
 								+ clientNews.getSkills() + "') as integer)=1");
 				repository.list(params).forEach(e -> {
-					notificationService.sendNotificationSync(null,
+					notificationService.sendNotification(null,
 							repository.one(Contact.class,
 									(BigInteger) e.get("contact.id")),
 							TextId.notification_clientNews, "news=" + clientNews.getId(),
 							clientNews.getSource() + ": " + clientNews.getDescription());
 				});
-			} catch (final Exception e) {
-				try {
-					clientNews.setNotified(false);
-					repository.save(clientNews);
-				} catch (final Exception e1) {
-				}
-				throw new RuntimeException(e);
-			}
+			});
 		}
 	}
 
