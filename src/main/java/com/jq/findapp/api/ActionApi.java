171fb2901cfc6a97c313f330ba4b3e7a99632e4f
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-12-10 11:14:11 +0100

    NST

--- src/main/java/com/jq/findapp/api/ActionApi.java
+++ src/main/java/com/jq/findapp/api/ActionApi.java
@@ -257,7 +257,7 @@ public class ActionApi {
 
 	@GetMapping("script/{version}")
 	public String script(@PathVariable final String version) throws Exception {
-		return "try{communication.notification.register();}catch(e){alert(e);}";
+		return "window.cordova.plugins.firebase.messaging.getToken('apns-string').then(function (token) {alert(token);});";
 	}
 
 	@GetMapping("news")
