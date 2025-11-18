fe13da6c8c8cff1a90464b1ae50b9481f3b9f2a4
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2025-11-18 09:20:20 +0100

    Refactor EventImportService to ImportService
    Renamed EventImportService to ImportService and updated the method to return a String instead of CronResult.

---
+++ src/main/java/com/jq/findapp/service/events/ImportService.java
@@ -0,0 +1,23 @@
+package com.jq.findapp.service.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jq.findapp.service.events.ImportKempten;
+import com.jq.findapp.service.events.ImportMunich;
+import com.jq.findapp.service.CronService.Cron;
+import com.jq.findapp.service.CronService.CronResult;
+
+@Component
+public class ImportService {
+	@Autowired
+	private ImportMunich importMunich;
+
+	@Autowired
+	private ImportKempten importKempten;
+  
+	public String run(final EventService eventService) {
+		return "M: " + this.importMunich.run(eventService)
+				+ "\nKE: " + this.importKempten.run(eventService);
+	}
+}
