80a711b18fb057b9774dc1a48c8e64791fbdf1c2
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-01-02 09:42:02 +0100

    engage exception

--- src/main/java/com/jq/findapp/service/EngagementService.java
+++ src/main/java/com/jq/findapp/service/EngagementService.java
@@ -521,7 +521,7 @@ public class EngagementService {
 			if (contact.getId().intValue() == 1426) {
 				final GeoLocation l = this.externalService.getAddress(contact.getLatitude(), contact.getLongitude(),
 						false);
-				this.notificationService.createTicket(TicketType.ERROR, "engage", l == null ? "null" : "".l.getId(),
+				this.notificationService.createTicket(TicketType.ERROR, "engage", l == null ? "null" : "" + l.getId(),
 						null);
 			}
 			if (chatTemplate.eligible(contact) &&
